6b145fe2a13ccdd7db030c0b9616940645ea32ee
Layla Hammoud <laylahammoud63@gmail.com>
2023-10-26 23:01:46 +0300

    remove the useless lines in App

---
+++ src/components/BookCard/BookCard.js
@@ -0,0 +1,15 @@
+import { BookCover } from "../../assets/images/bookcover.jpg";
+const BookCard = () => {
+  return (
+    <>
+      <figure>
+        <img src={BookCover} alt="Book cover" />
+        <figcaption>
+          <p>Memory</p>
+          <p>Hana Hope</p>
+        </figcaption>
+      </figure>
+    </>
+  );
+};
+export default BookCard;
