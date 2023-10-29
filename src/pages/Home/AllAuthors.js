73b79c44f39bd342587b34d7f6464568ee3ff39e
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-10-29 06:20:43 +0200

    navbar done with responsive designe, route, active link

---
+++ src/pages/Home/AllAuthors.js
@@ -0,0 +1,208 @@
+import React from "react";
+
+const AllAuthors = () => {
+  return (
+    <div>
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+      <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
+        perferendis voluptate quo harum tempora, corporis cumque architecto?
+        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
+        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
+        pariatur officiis qui reiciendis alias at, repudiandae esse
+        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
+        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
+        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
+        eos accusantium hic temporibus placeat numquam esse! Architecto
+        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
+        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
+        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
+        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
+        consequatur illo deserunt hic!
+      </div>
+      <br />
+    </div>
+  );
+};
+
+export default AllAuthors;
