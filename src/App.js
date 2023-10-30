448b9a01835ac858724d52ec83169129b849940c
louai <louaibaghdadi27@gmail.com>
2023-10-30 10:56:48 +0200

    landing page finished

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Footer from "./components/Footer/Footer.js";
 import HeroSection from "./components/HeroSection/HeroSection.js";
+import Contents from "./components/Contents/Contents.js";
 
 
 function App() {
   return (
     <div className="App">
       <HeroSection/>
+      <Contents/>
       <Footer />
     </div>
   );
