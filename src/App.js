040b48934750a6e3e37dbd41c4b5e536219830d3
Layla Hammoud <laylahammoud63@gmail.com>
2023-10-31 14:39:21 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
-import AddBookForm from "./pages/AddBookForm/AddBookForm";
 // we have to import pages component in order to work in route
 import { Route, Routes } from "react-router-dom";
-import AllAuthors from "./pages/Home/AllAuthors";
-import SingleBook from "./pages/SingleBook/SingleBook";
-import SingleAuther from "./pages/SingleAuther/SingleAuther";
-import AddAuther from "./pages/AddAutherForm/AddAutherForm";
+import AllAuthors from "./components/AllAuthors/AllAuthors";
+import AllBooks from "./components/AllBooks/AllBooks";
+import Footer from "./components/Footer/Footer";
+//import SingleBook from "./pages/SingleBook/SingleBook";
+//import SingleAuther from "./pages/SingleAuther/SingleAuther";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        {/* <Route path="/" element={<Hom />}>
-          <Route path="/AllBooks" element={<AllBooks />}></Route> */}
-        <Route path="/AllAuthors" element={<AddAuther />}></Route>
+        {/* <Route path="/" element={<Hom />}>*/}
+          <Route path="/AllBooks" element={<AllBooks />}></Route> 
+        <Route path="/AllAuthors" element={<AllAuthors />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
