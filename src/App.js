b837824f8e560f18ea5d6e035500d3207f515dd5
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-10-31 09:46:50 +0200

    page all books done with responsive designe

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-
 import React from "react";
-import NavBar from "./components/NavBar/NavBar";
 
 // we have to import pages component in order to work in route
 import { Route, Routes } from "react-router-dom";
-import AllAuthors from "./pages/Home/AllAuthors";
+import NavBar from "./components/NavBar/NavBar";
+import AllAuthors from "./components/AllAuthors/AllAuthors";
+import AllBooks from "./components/AllBooks/AllBooks";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        {/* <Route path="/" element={<Hom />}>
-          <Route path="/AllBooks" element={<AllBooks />}></Route> */}
+        {/* <Route path="/" element={<Hom />}> */}
+        <Route path="/AllBooks" element={<AllBooks />}></Route>
         <Route path="/AllAuthors" element={<AllAuthors />}></Route>
+        {/* <Route path="/AuthorDetails/:id" element={<AuthorDetails />}></Route> */}
+        {/* <Route path="/BookDetails/:id" element={<BookDetails />}></Route> */}
       </Routes>
+      {/* <Footer /> */}
     </div>
   );
 }
