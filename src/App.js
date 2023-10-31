affd91d19107f2dea8e1357c21f5d3cbc5763826
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-10-31 14:30:24 +0200

    finish all authors page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 // we have to import pages component in order to work in route
 import { Route, Routes } from "react-router-dom";
-import AllAuthors from "./pages/Home/AllAuthors";
+import AllAuthors from "./components/AllAuthors/AllAuthors";
+import AllBooks from "./components/AllBooks/AllBooks";
+import Footer from "./components/Footer/Footer";
 import SingleBook from "./pages/SingleBook/SingleBook";
 import SingleAuther from "./pages/SingleAuther/SingleAuther";
 function App() {
@@ -10,10 +12,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        {/* <Route path="/" element={<Hom />}>
-          <Route path="/AllBooks" element={<AllBooks />}></Route> */}
+        {/* <Route path="/" element={<Hom />}> */}
+        <Route path="/AllBooks" element={<AllBooks />}></Route>
         <Route path="/AllAuthors" element={<AllAuthors />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/AllAuthors/AllAuthors.js
+++ src/components/AllAuthors/AllAuthors.js
@@ -1,206 +1,216 @@
-import React from "react";
+import React, { useState } from "react";
+import AllBooksStyle from "../AllBooks/AllBooks.module.css";
+import AuthorCover from "../../assets/images/james-clear-headshot-v1-e1538685972972.jpg";
+import filter from "../../assets/icons/filter.png";
 
+let rating = 4;
 const AllAuthors = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleClick = () => {
+    setMenuOpen(!menuOpen);
+    console.log("clicked"); // Toggle the menuOpen state
+  };
   return (
     <div>
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
+      <form className={AllBooksStyle.bookSearch}>
+        <input id="search" className={AllBooksStyle.inputSearch} type="text" />
+        <button for="#search" className={AllBooksStyle.searchButton}>
+          search
+        </button>
+      </form>
+
+      <div className={AllBooksStyle.filterCategories}>
+        <input
+          type="text"
+          id="Categories"
+          name="Categories "
+          value="Search for Categories"
+        />
+        <button for="#Categories" onClick={handleClick}>
+          <img src={filter} alt="filter" />
+        </button>
       </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
+      <div className={AllBooksStyle.booksContainer}>
+        <div
+          // className={AllBooksStyle.booksCategory}
+          className={`${AllBooksStyle.booksCategory} 
+          ${menuOpen ? AllBooksStyle.open : ""}
+          `}
+        >
+          <h2>Categories</h2>
+          <div className={AllBooksStyle.bookCheckbox}>
+            <input
+              type="checkbox"
+              id="fiction"
+              name="fiction "
+              value="fiction"
+            />
+            <label for="fiction"> fiction</label>
+            <br></br>
+          </div>
+          <div className={AllBooksStyle.bookCheckbox}>
+            <input
+              type="checkbox"
+              id="nonFiction"
+              name="nonFiction"
+              value="nonFiction"
+            />
+            <label for="nonFiction"> non-Fiction</label>
+            <br></br>
+          </div>
+          <div className={AllBooksStyle.bookCheckbox}>
+            <input
+              type="checkbox"
+              id="romance"
+              name="romance"
+              value="romance"
+            />
+            <label for="romance"> Romance</label>
+            <br></br>
+          </div>
+        </div>
+        <div className={AllBooksStyle.booksList}>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+          <figure className={AllBooksStyle.figure}>
+            <img
+              src={AuthorCover}
+              alt="Book cover"
+              className={AllBooksStyle.image}
+            />
+            <figcaption className={AllBooksStyle.figcaption}>
+              <p>
+                <strong>Memory</strong>
+              </p>
+              <p>Hana Hope</p>
+              <div>
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span
+                    key={star}
+                    className={`${rating >= star ? AllBooksStyle.filled : ""}`}
+                  >
+                    &#9733;
+                  </span>
+                ))}
+              </div>
+            </figcaption>
+          </figure>
+        </div>
       </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
-      <div>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam
-        perferendis voluptate quo harum tempora, corporis cumque architecto?
-        Earum magnam possimus unde voluptatem, ad tempore minus? Eius itaque
-        illo omnis aut! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Dolores a veniam totam quibusdam ipsa? Eligendi molestias, debitis
-        pariatur officiis qui reiciendis alias at, repudiandae esse
-        reprehenderit magnam consequatur fugit quibusdam temporibus. Eum ut,
-        nemo voluptate vitae voluptatum, aut optio modi quisquam accusamus
-        aliquam nisi molestiae, nobis ad necessitatibus sed ea asperiores labore
-        eos accusantium hic temporibus placeat numquam esse! Architecto
-        perferendis aliquid necessitatibus, vel magnam explicabo maiores dolor
-        praesentium modi, reprehenderit laborum quasi quod ea accusamus ex amet?
-        Voluptas repellat assumenda obcaecati, veniam similique inventore, nulla
-        molestiae quidem delectus ipsum tenetur a! Inventore atque, in obcaecati
-        consequatur illo deserunt hic!
-      </div>
-      <br />
     </div>
   );
 };
