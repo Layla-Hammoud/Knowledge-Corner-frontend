15d31c985ce9cfda307d6e80de47f4b69a981fac
Layla Hammoud <laylahammoud63@gmail.com>
2023-11-03 02:45:21 +0200

    finish the add and edit functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import AllBooks from "./components/AllBooks/AllBooks";
 import Footer from "./components/Footer/Footer";
 //import SingleBook from "./pages/SingleBook/SingleBook";
 //import SingleAuther from "./pages/SingleAuther/SingleAuther";
+import AddEditAutherForm from "./pages/AddEditAutherForm/AddEditAutherForm";
 import Landing from "./pages/Landing/Landing";
+const key='Add'
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Landing />}></Route>
+        <Route path="/" element={<AddEditAutherForm />}></Route>
         <Route path="/AllBooks" element={<AllBooks />}></Route> 
         <Route path="/AllAuthors" element={<AllAuthors />}></Route>
       </Routes>
