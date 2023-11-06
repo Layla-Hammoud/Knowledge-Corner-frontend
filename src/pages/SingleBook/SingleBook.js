e3a4612bbee2bde64b69b7b3c8cd5b363abbb7c0
Layla Hammoud <laylahammoud63@gmail.com>
2023-11-06 23:12:35 +0200

    add data to single book

--- src/pages/SingleBook/SingleBook.js
+++ src/pages/SingleBook/SingleBook.js
@@ -1,56 +1,77 @@
 import BookCard from "../../components/BookCard/BookCard";
 import BookCover from "../../assets/images/bookcover.jpg";
-import style from './SingleBook.module.css'
+import style from "./SingleBook.module.css";
 import Stars from "../../components/Stars/Stars";
 function SingleBook() {
-  let rating=3;
+
+  function formatDate(inputDate) {
+    const date = new Date(inputDate);
+    const formattedDate = date.toISOString().split("T")[0];
+
+    return formattedDate;
+  }
+  let book = {
+    _id: "6548f5bb0a13fcd6c98d1ae3",
+    title: "soul",
+    ISBN: "098876",
+    publicationDate: "2023-11-01T00:00:00.000Z",
+    description:
+      "Good thinkers are always in demand. A person who knows how may always have a job, but the person who knows why will always be his boss.",
+    nbPages: 885,
+    authorId: "653774a9ecc137fc0d211526",
+    categoryId: "65362ad892ebe34bdbce5170",
+    image: "image-1699280315765-963972417.jpg",
+    language: "English",
+    rating: 1,
+    createdAt: "2023-11-06T14:18:35.796Z",
+    updatedAt: "2023-11-06T14:18:35.796Z",
+    __v: 0,
+  };
   return (
     <section className={style.singleBookContainer}>
       <div className={style.mainInfoContainer}>
         <article className={style.imgContainer}>
-          <img className={style.image} src={BookCover} alt="Book cover"/>
-          <div className={style.StarSingleContainer}> 
+          <img className={style.image} src={`http://localhost:4000/images/${book.image}`} alt="Book cover" />
+          <div className={style.StarSingleContainer}>
             {[1, 2, 3, 4, 5].map((star) => (
-              <Stars key={star} stars={star} rating={rating}/>
+              <Stars key={star} stars={star} rating={book.rating} />
             ))}
           </div>
         </article>
         <article className={style.infoContainer}>
-            <h3 className={style.info}>Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones</h3>
-            <h4 className={style.info}>James Clear</h4>
+          <h3 className={style.info}>
+            {book.title}
+          </h3>
+          <h4 className={style.info}>need to check</h4>
           <p className={style.info}>
-            Genre:<span className={style.spantext}>Self help</span>
+            Genre:<span className={style.spantext}>need to check</span>
           </p>
           <p className={style.info}>
-            Language :<span className={style.spantext}>English</span>{" "}
+            Language :<span className={style.spantext}>{book.language}</span>{" "}
           </p>
           <p className={style.info}>
-            Published Date :<span className={style.spantext}>October 16, 2018</span>
+            Published Date :
+            <span className={style.spantext}>{formatDate(book.publicationDate)}</span>
           </p>
           <p className={style.info}>
-            Pages :<span className={style.spantext}> 544</span>
+            Pages :<span className={style.spantext}>{book.nbPages}</span>
           </p>
           <p className={style.info}>
-            ISBN :<span className={style.spantext}>0735211302</span>
+            ISBN :<span className={style.spantext}>{book.ISBN}</span>
           </p>
           <p className={style.info}>
-            Atomic Habits will reshape the way you think about progress and
-            success, and give you the tools and strategies you need to transform
-            your habits- -whether you are a team looking to win a championship,
-            an organization hoping to redefine an industry, or simply an
-            individual who wishes to quit smoking, lose weight, reduce stress,
-            or achieve any other goal.
+            {book.description}
           </p>
         </article>
       </div>
       <h4 className={style.suggestedBookTitle}>Readers also enjoyed</h4>
       <aside className={style.suggestedBook}>
-        <BookCard isSmall={true}/>
-        <BookCard isSmall={true}/>
-        <BookCard isSmall={true}/>
-        <BookCard isSmall={true}/>
-        <BookCard isSmall={true}/>
-        <BookCard isSmall={true}/>
+        <BookCard isSmall={true} />
+        <BookCard isSmall={true} />
+        <BookCard isSmall={true} />
+        <BookCard isSmall={true} />
+        <BookCard isSmall={true} />
+        <BookCard isSmall={true} />
       </aside>
     </section>
   );
--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import AllAuthors from '../components/AllAuthors/AllAuthors';
 import AllBooks from "../components/AllBooks/AllBooks";
 import SingleBook from '../pages/SingleBook/SingleBook'
-import SingleAuther from '../pages/SingleAuther/SingleAuther'
 import Landing from "../pages/Landing/Landing";
 import Dashboard from '../pages/Admin/dashboard'
 import UserOutlet from "../Outlet/userOutlet";
@@ -14,7 +13,7 @@ function AppRoutes() {
     <Routes>
 
         <Route exact path="/" element={<UserOutlet />}>
-          <Route exact path="/" element={<SingleAuther />}> </Route>
+          <Route exact path="/" element={<Landing />}> </Route>
           <Route path="/AllBooks" element={<AllBooks />}></Route> 
           <Route path="/AllAuthors" element={<AllAuthors />}></Route>
           <Route path="/SingleBook" element={<SingleBook/>}></Route>
